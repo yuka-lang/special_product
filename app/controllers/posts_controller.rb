04c927f08f96128c55420e058d24245ed23b2a38
yuka-lang <yuaaasly116@icloud.com>
2022-01-17 13:13:11 +0000

    [add]チェックボックス

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,32 @@
 class PostsController < ApplicationController
 
+  before_action :sidebar
+  before_action :ranking, only: [:index, :search, :search_tag]
+
   def new
     @post = Post.new
-    @tag_lists = Tag.all
+
+
+    tag_tops = PostTag.joins(:tag).group(:tag_id).select("tag_id, count(tag_id) AS count, tags.name").order("count desc").limit(5)
+
+    @top = []
+    tag_tops.each do |tag_top|
+      @top.push([tag_top.tag.id, tag_top.tag.name])
+    end
   end
 
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    @tag_lists = Tag.all
+
     # 受け取った値を,で区切って配列にする
     tag_list = params[:post][:names].split(',')
     if @post.save
-       @post.save_tag(tag_list)
+      # binding.irb
+      @post.save_tag(tag_list)
+      params[:post][:tag_ids].each do |tag_id|
+        PostTag.create(post_id: @post.id, tag_id: tag_id)
+      end
       redirect_to post_path(@post.id)
     else
       render :new
@@ -21,11 +35,6 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.page(params[:page]).reverse_order.per(6)
-    # Favoriteモデルから今週のデータを取り出し、数の多い順に並び替えたpost_idの値をPostモデルから探す(３位までを表示)
-    @week_post_favorite_ranks = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_week).order("count(post_id) DESC").limit(3).pluck(:post_id))
-    #上記と同じように月間のコメント数の多い順で表示する
-    @month_post_comment_ranks = Post.find(Comment.group(:post_id).where(created_at: Time.current.all_month).order("count(user_id) DESC").limit(3).pluck(:user_id))
-    @tag_lists = Tag.all
   end
 
   def show
@@ -34,22 +43,36 @@ class PostsController < ApplicationController
     @comments = @post.comments.all
     @post_tags = @post.tags
     @user = current_user
-    @tag_lists = Tag.all
+
   end
 
   def edit
     @post = Post.find(params[:id])
-    @tag_list = @post.tags.pluck(:name).join(',')
-    @tag_lists = Tag.all
+    @tag_list = @post.tags.pluck(:name) # .join(',')
+
+
+    tag_tops = PostTag.joins(:tag).group(:tag_id).select("tag_id, count(tag_id) AS count, tags.name").order("count desc").limit(5)
+
+    @top = []
+    top_for_edit = []
+    tag_tops.each do |tag_top|
+      @top.push([tag_top.tag.id, tag_top.tag.name])
+      top_for_edit.push(tag_top.tag.name)
+    end
+
+    @tag_list = (@tag_list - top_for_edit).join(',')
+
   end
 
   def update
     @post = Post.find(params[:id])
-    @tag_lists = Tag.all
     tag_list = params[:post][:names].split(',')
     if @post.update(post_params)
-       @post.save_tag(tag_list)
-       flash[:notice] = '更新に成功しました！'
+      @post.save_tag(tag_list)
+      params[:post][:tag_ids].each do |tag_id|
+        PostTag.create(post_id: @post.id, tag_id: tag_id)
+      end
+      flash[:notice] = '更新に成功しました！'
       redirect_to post_path(@post)
     else
       render :edit
@@ -71,12 +94,7 @@ class PostsController < ApplicationController
   # タグ検索画面
   def search_tag
     @tag = Tag.find(params[:tag_id])
-    @tag_lists = Tag.all
     @posts = @tag.posts.all
-     # Favoriteモデルから今週のデータを取り出し、数の多い順に並び替えたpost_idの値をPostモデルから探す
-    @week_post_favorite_ranks = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_week).order('count(post_id) DESC').limit(3).pluck(:post_id))
-    #上記と同じように月間のコメント数の多いランキングを表示する
-    @month_post_comment_ranks = Post.find(Comment.group(:post_id).where(created_at: Time.current.all_month).order("count(user_id) DESC").limit(3).pluck(:user_id))
   end
 
   # 検索機能
@@ -84,18 +102,41 @@ class PostsController < ApplicationController
     # キーワードを探しpaginationを利用
     @posts = Post.search(params[:keyword]).page(params[:page]).per(6)
     @keyword = params[:keyword]
-    @tag_lists = Tag.all
-     # Favoriteモデルから今週のデータを取り出し、数の多い順に並び替えたpost_idの値をPostモデルから探す
-    @week_post_favorite_ranks = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_week).order('count(post_id) DESC').limit(3).pluck(:post_id))
-    #上記と同じように月間のコメント数の多いランキングを表示する
-    @month_post_comment_ranks = Post.find(Comment.group(:post_id).where(created_at: Time.current.all_month).order("count(user_id) DESC").limit(3).pluck(:user_id))
     render "index"
+  end
+
+  def sidebar
+    @tag_lists = PostTag.joins(:tag)
+                        .group(:tag_id)
+                        .select("tag_id, count(tag_id) AS count, tags.name")
+                        .order("count desc")
+                        .limit(5)
+  end
+
+  def ranking
+    # Favoriteモデルから今週のデータを取り出し、数の多い順に並び替えたpost_idの値をPostモデルから探す(３位までを表示)
+    @week_post_favorite_ranks = Post.find(
+      Favorite.group(:post_id)
+              .where(created_at: Time.current.all_week)
+              .order("count(post_id) DESC")
+              .limit(3)
+              .pluck(:post_id)
+    )
+
+    #上記と同じように月間のコメント数の多い順で表示する
+    @month_post_comment_ranks = Post.find(
+      Comment.group(:post_id)
+            .where(created_at: Time.current.all_month)
+            .order("count(user_id) DESC")
+            .limit(3)
+            .pluck(:post_id)
+    )
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :image, :prefectures, :season, :word, :introduction)
+    params.require(:post).permit(:title, :image, :prefectures, :season, :word, :introduction, tag_ids: [])
   end
 
 end
